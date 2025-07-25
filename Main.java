8a6ef0e3d10d995c67f68da5d3517fe0976ff3a8
Abhijeet <abhiaundhakar@gmail.com>
2025-07-25 22:13:11 +0530

    Added student data

--- Main.java
+++ Main.java
@@ -1,9 +1,21 @@
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Main{
     public static void main(String[] args) {
         System.out.println("This is testing of jenkins");
         System.out.println(new Date());
         System.out.println("This is third line");
+
+        List<Student> list = new ArrayList<>(List.of(
+            new Student("Abhijeet", 23),
+            new Student("Santosh", 23),
+            new Student("Pradeep", 23)
+        ));
+
+        System.out.println(list);
+
+       
     }
 }
